27d0d549d115556113aaeb5fe6a6fffdab423704
webdesigntuts <hostelidis99@gmail.com>
2022-01-16 21:24:13 +0200

    commit

--- frontend/src/components/PageContainer.jsx
+++ frontend/src/components/PageContainer.jsx
@@ -2,7 +2,7 @@ import styles from "../styles/PageContainer.module.scss";
 import { motion } from "framer-motion";
 import { opacityAnimation } from "../settings/animations";
 
-const PageContainer = ({ children }) => {
+const PageContainer = ({ children, classOption }) => {
   return (
     <motion.div
       className={styles.container}
@@ -11,9 +11,13 @@ const PageContainer = ({ children }) => {
       animate="show"
       exit="hidden"
     >
-      <div className={styles.content}>{children}</div>
+      <div className={`styles.content ${classOption}`}>{children}</div>
     </motion.div>
   );
+};
+
+PageContainer.defaultProps = {
+  classOption: undefined,
 };
 
 export default PageContainer;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,12 @@
 import styles from "../styles/Home.module.scss";
-import { opacityAnimation } from "../settings/animations";
-import { motion } from "framer-motion";
+import PageContainer from "../components/PageContainer";
 
 const Home = () => {
   return (
-    <motion.div
-      className={styles.home}
-      variants={opacityAnimation}
-      initial="hidden"
-      animate="show"
-      exit="hidden"
-    >
+    <PageContainer classOption={styles.home}>
       <h1>My Name</h1>
       <h3>FrontEnd-developer</h3>
-    </motion.div>
+    </PageContainer>
   );
 };
 
